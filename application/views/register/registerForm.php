2bc01594eae3c786da61a018c278d8e2662aede7
markusfluer <markus.fluer@limesurvey.org>
2016-08-02 15:05:14 +0200

    Dev: corrected Registartion to horizontal forms

--- application/views/register/registerForm.php
+++ application/views/register/registerForm.php
@@ -1,33 +1,45 @@
-<?php echo CHtml::form($urlAction,'post',array('id'=>'limesurvey', 'role' => 'form')); ?>
+<?php echo CHtml::form($urlAction,'post',array('id'=>'limesurvey', 'role' => 'form', 'class' => 'form-horizontal')); ?>
     <input type="hidden" name="lang" value="<?php echo $sLanguage; ?>" id="register_lang" />
     <div class='form-group col-xs-12'>
-        <label for='register_firstname'><?php eT("First name"); ?></label>
-        <?php echo CHtml::textField('register_firstname', $sFirstName,array('id'=>'register_firstname','class'=>'form-control input-sm')); ?>
+        <label for='register_firstname' class='control-label col-sm-2'><?php eT("First name"); ?></label>
+        <div class="col-sm-10">
+            <?php echo CHtml::textField('register_firstname', $sFirstName,array('id'=>'register_firstname','class'=>'form-control input-sm')); ?>
+        </div>
     </div>
     <div class='form-group col-xs-12'>
-        <label for='register_lastname'><?php eT("Last name"); ?></label>
+        <label for='register_lastname' class='control-label col-sm-2'><?php eT("Last name"); ?></label>
+        <div class="col-sm-10">
         <?php echo CHtml::textField('register_lastname', $sLastName,array('id'=>'register_lastname','class'=>'form-control input-sm')); ?>
+        </div>
     </div>
     <div class='form-group col-xs-12'>
-        <label for='register_email'><?php eT("Email address"); ?></label>
+        <label for='register_email' class='control-label col-sm-2'><?php eT("Email address"); ?></label>
+        <div class="col-sm-10">
         <?php echo CHtml::textField('register_email', $sEmail,array('id'=>'register_email','class'=>'form-control input-sm','required'=>'required')); ?>
+        </div>
     </div>
     <?php foreach($aExtraAttributes as $key=>$aExtraAttribute){ ?>
         <div class='form-group col-xs-12'>
-            <label for="register_<?php echo $key; ?>"><?php echo $aExtraAttribute['caption']; ?><?php echo $aExtraAttribute['mandatory'] == 'Y' ? '*' : ""; ?></label>
+            <label for="register_<?php echo $key; ?>" class='control-label col-sm-2'><?php echo $aExtraAttribute['caption']; ?><?php echo $aExtraAttribute['mandatory'] == 'Y' ? '*' : ""; ?></label>
+            <div class="col-sm-10">
             <?php echo CHtml::textField("register_{$key}", $aAttribute[$key],array('id'=>"register_{$key}",'class'=>'form-control input-sm')); ?>
+            </div>
         </div>
     <?php } ?>
     <?php if($bCaptcha){ ?>
     <div class='form-group col-xs-12'>
-        <label for='loadsecurity'><?php eT("Security Question"); ?></label>
+        <label for='loadsecurity' class='control-label col-sm-2'><?php eT("Security Question"); ?></label>
+        <div class="col-sm-4">
         <?php $this->widget('CCaptcha',
                 array(
                     'buttonOptions'=>array('class'=> 'btn btn-xs btn-info'),
                     'buttonType' => 'button',
                     'buttonLabel' => gt('Reload Captcha')
                 )); ?>
-        <?php echo CHtml::textField('loadsecurity', '',array('id'=>'loadsecurity','class'=>'form-control input-sm','required'=>'required')); ?>
+        </div>
+        <div class="col-sm-6">
+            <?php echo CHtml::textField('loadsecurity', '',array('id'=>'loadsecurity','class'=>'form-control input-sm','required'=>'required')); ?>
+        </div>
     </div>
     <?php } ?>
     <div class='form-group col-xs-12'>
